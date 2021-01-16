820967d9bb5cf7ade53fcb10e5d4f59d9428fbfe
Baniene <76951522+Baniene@users.noreply.github.com>
2021-01-16 13:55:26 +0200

    commit

---
+++ main.js
@@ -0,0 +1,38 @@
+//vienos eilutes komentaras
+
+/*
+keletos
+eiluciu
+komentaras
+*/
+
+
+// jei noresime ka nors atsisapusdinti, tai naudojam console.log()
+console.log(2021); 
+console.log('Labas rytas');
+
+
+/*
+Reiksmiu tipai:
+    number
+    string (tekstas)
+    boolean (logine reiksme)
+    array (sarasas)
+    object (objektas)
+*/
+
+/*
+kintamuju inicijavimo budai:
+    const (pagrindinis, kuri reikia rinktis by defalt (pagal nutylejima))
+    left (jei info keiciaisi)
+    var (nenaudoti)
+*/
+
+//inicijuoti kintamaji, kuris niekada nesikeis ir bus skaicius 2021
+const metai = 2021;
+
+//incijuoti kintamaji, kuris niekada nesikeis ir bus tekstas Petras
+const vardas = 'Petras';
+
+console.log(metai);
+console.log(vardas); 
